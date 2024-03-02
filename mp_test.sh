05498a928b252a00f7021f7b1e8de09056abfd16
TheJavaNoob <3393746726@qq.com>
2024-03-02 17:05:40 +0800

    Added script

---
+++ mp_test.sh
@@ -0,0 +1,13 @@
+#lc3as mp2.asm
+for filename in *; do
+      if [[ "$filename" != *"."* ]] && [[ "$filename" != *"out" ]]; then
+            echo "Testing $filename"
+            lc3sim -s $filename > my_out
+            diff my_out ${filename}out > /dev/null 2>&1
+            if [[ $? -eq 0 ]]; then
+                  echo "Test succeeded\n"
+            else
+                  echo "Test failed\n"
+            fi
+      fi
+done
