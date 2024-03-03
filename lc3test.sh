a54d8d03227167d77602b131f2aab8d919eb9b76
Joeyhou <runchen.23@intl.zju.edu.cn>
2024-03-03 09:10:28 +0800

    delete line feed

--- lc3test.sh
+++ lc3test.sh
@@ -5,15 +5,15 @@ for filename in *; do
             lc3as $filename
       fi
       } &> /dev/null
-
+      
       if [[ "$filename" != *"."* ]] && [[ "$filename" != *"out" ]] && [[ "$filename" != *"myresult"* ]]; then
             echo "Testing $filename"
             lc3sim -s $filename > my_out
             diff my_out ${filename}out > /dev/null 2>&1
             if [[ $? -eq 0 ]]; then
-                  echo "Test succeeded\n"
+                  echo "Test succeeded"
             else
-                  echo "Test failed\n"
+                  echo "Test failed"
             fi
       fi
 done
