d2d0ef4292540b64b531e6743cd5b1325bad51ab
Joeyhou <runchen.23@intl.zju.edu.cn>
2024-03-03 10:26:29 +0800

    fixed bugs

--- lc3test.sh
+++ lc3test.sh
@@ -1,19 +1,33 @@
-#lc3as mp2.asm
 for filename in *; do
-      {
       if [[ "$filename" == *".asm" ]]; then
-            lc3as $filename
-      fi
-      } &> /dev/null
-      
-      if [[ "$filename" != *"."* ]] && [[ "$filename" != *"out" ]] && [[ "$filename" != *"myresult"* ]]; then
-            echo "Testing $filename"
-            lc3sim -s $filename > my_out
-            diff my_out ${filename}out > /dev/null 2>&1
-            if [[ $? -eq 0 ]]; then
-                  echo "Test succeeded"
-            else
-                  echo "Test failed"
+            output=$(lc3as $filename 2>&1)
+            # Check if the word 'fail' is in the output
+            if echo "$output" | grep -q ":"; then
+                  echo "$output"
+                  echo "Aborting program. "
+                  # Terminate the program
+                  exit 
+            # If 'fail' is not found, do not display the output and continue
             fi
       fi
 done
+
+ifsuccess=1
+for filename in *; do
+      if [[ "$filename" != *"."* ]] && [[ "$filename" != *"out" ]] && [[ "$filename" != *"myresult"* ]]; then
+            lc3sim -s $filename > my_out
+            diff my_out ${filename}out > /dev/null 2>&1
+            if [[ $? -eq 0 ]]; then
+                  echo "testing $filename...passed"
+            else
+                  echo "testing $filename...FAILED"
+                  ifsuccess=0
+            fi
+      fi
+done
+
+if [ $ifsuccess == 1 ]; then
+      echo "All tests passed successfully!"
+else
+      echo "Some tests failed."
+fi
